91980d0497a721dbbf955e02c89d2294f387f021
Daniel Findell <circa.1991@gmail.com>
2022-08-11 20:03:00 -0500

    Updated bucket sizes

--- src/metrics.rs
+++ src/metrics.rs
@@ -4,7 +4,7 @@ use std::time::Instant;
 
 pub fn setup_metrics_recorder() -> PrometheusHandle {
     const EXPONENTIAL_SECONDS: &[f64] = &[
-        0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1.0, 2.5, 5.0, 10.0,
+        0.001, 0.01, 0.1, 0.25, 0.5, 1.0, 2.5, 5.0, 10.0, 30.0, 60.0,
     ];
 
     PrometheusBuilder::new()
