e1963b6c36509714f230997e5b9cc702ba3ae956
Daniel Findell <Daniel.Findell@chrobinson.com>
2023-06-14 14:56:06 -0500

    Added metrics timeout

--- src/metrics.rs
+++ src/metrics.rs
@@ -1,13 +1,19 @@
 use axum::{http::Request, middleware::Next, response::IntoResponse};
 use hyper::body::HttpBody;
 use metrics_exporter_prometheus::{Matcher, PrometheusBuilder, PrometheusHandle};
+use metrics_util::MetricKindMask;
 use std::time::Instant;
+use core::time::Duration;
 
 pub fn setup_metrics_recorder() -> PrometheusHandle {
     const EXPONENTIAL_SECONDS: &[f64] =
         &[0.001, 0.01, 0.1, 0.25, 0.5, 1.0, 2.5, 5.0, 10.0, 30.0, 60.0];
 
     PrometheusBuilder::new()
+        .idle_timeout(
+            MetricKindMask::COUNTER | MetricKindMask::GAUGE,
+            Some(Duration::from_secs(120)),
+        )
         .set_buckets_for_metric(
             Matcher::Full("proxima_requests_duration_seconds".to_string()),
             EXPONENTIAL_SECONDS,
